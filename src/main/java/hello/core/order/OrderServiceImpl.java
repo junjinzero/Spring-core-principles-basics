39c99063e0c988f9b6c8459e5fb2bef465f840cc
junjinzero <progressnull@gmail.com>
2024-09-22 16:18:13 +0900

    ## 10강 주문과 할인 도메인 개발 - OrderService : 주문 결과를 반환하는 인터페이스 - OrderServiceImpl :: 할인 정책이 변경되어도 discountPolicy 에서 변경된 정책을 적용해서 결과만 던져주고, 해당 구현체에서는 반환된 금액만 사용하면 됨   - -> 단일 책임 원칙이 잘 구현된 형 (주문 구현체까지 고칠 필요가 없으므로) - 주문 생성 요청이 오면, 회원 정보를 먼저 조회하고, 해당 멤버의 등급을 찾아 할인 정책을 적용한 결과 금액을 받아 온다.

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // 주문 생성 요청이 오면, 회원 정보를 먼저 조회하고,
+    // 해당 멤버의 등급을 찾아 할인 정책을 적용한 결과 금액을 받아 온다.
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
