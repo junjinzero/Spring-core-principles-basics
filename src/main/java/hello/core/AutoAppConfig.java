a34fe83f86e2ae2af3c369625c0675d2947519cf
junjinzero <progressnull@gmail.com>
2024-10-15 08:37:04 +0900

    ## 36강 탐색 위치와 기본 스캔 대상 모든 자바 클래스를 다 컴포넌트 스캔하면 시간이 오래 걸린다. 그래서 꼭 필요한 위치부터 탐색하도록 시작 위치를 지정할 수 있다.

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //  스프링이 얘를 읽어서 자동으로 등록하도록 하는 어노테이션
 @ComponentScan (
+        basePackages = "hello.core",  // 여기서부터 시작해서 하위 패키지를 찾는다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
