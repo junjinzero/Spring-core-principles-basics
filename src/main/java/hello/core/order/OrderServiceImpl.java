4ef4a4306d4cc84618d58a3b43121d4493ae1dd6
junjinzero <progressnull@gmail.com>
2024-10-23 09:29:46 +0900

    ## 42강 롬복과 최신 트렌드 막상 개발을 해보면, 대부분이 다 불변이고, 생성자에 final 키워드를 사용하게 됨 <br> --> 필드 주입처럼 편리하게 사용하는 방법은 없을까?
    > 최근에는 생성자를 딱 1개 두고, `@Autowired` 를 생략하는 방법을 주로 사용한다. <br>
    > 여기에 Lombok 라이브러리의`@RequiredArgsConstructor` 함께 사용하면 기능은 다 제공하면서, 코드는 깔끔하게 사용할 수 있다

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,18 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     // 주문 생성 요청이 오면, 회원 정보를 먼저 조회하고,
     // 해당 멤버의 등급을 찾아 할인 정책을 적용한 결과 금액을 받아 온다.
