c1086e72233d7f4b2ff14ae849e2af70ae6821b1
junjinzero <progressnull@gmail.com>
2025-06-19 14:48:51 +0900

    ## 50강 인터페이스 빈 등록 초기화, 소멸 메서드 ## 51강 애노테이션 @PostConstruct, @PreDestroy - 그냥 이걸 사용하면 된다 - 코드를 고칠 수 없는 외부 라이브러리를 초기화, 종료해야 하면, `@Bean` 의 `initMethod`, `destoryMethod` 를 사용하자

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,10 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
@@ -29,17 +30,18 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
         // 프로퍼티 세팅이 끝나면 (의존관계 주입이 끝나면 호출해주겠다)
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    @PreDestroy
+    public void close() {
         // bean 이 종료될 때 호출
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
